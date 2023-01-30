775111a5adbd1c3423b301c0e2a7135ded006af1
xenolito <oscar.rey.tajes@gmail.com>
2023-01-30 19:50:10 +0100

    Initial setup

---
+++ src/Experience.jsx
@@ -0,0 +1,47 @@
+import { OrbitControls } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+// import { useControls } from 'leva'
+// import { Perf } from 'r3f-perf'
+import React, { useRef } from 'react'
+
+const Experience = () => {
+
+    const cube = useRef()
+
+    // const { showPerf } = useControls('Performance', {
+    //     showPerf: true
+    // })
+
+    // const { cubeWireFrame } = useControls('Cube', {
+    //     cubeWireFrame: false,
+    // })
+
+    useFrame((state, delta) => {
+        cube.current.rotation.y += delta
+        // console.log(delta)
+    })
+
+    return (
+        <>
+            {/* { showPerf &&  <Perf position="top-left" /> } */}
+
+            <OrbitControls makeDefault />
+            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+            <ambientLight intensity={ 0.5 } />
+
+            <mesh ref={cube}>
+                <boxGeometry />
+                {/* <meshNormalMaterial wireframe={ cubeWireFrame } /> */}
+                <meshStandardMaterial color="mediumpurple" wireframe={ false } />
+            </mesh>
+
+            <mesh rotation-x={- Math.PI * 0.5} position-y={ -1 }>
+                <planeGeometry args={[10, 10]} />
+                <meshStandardMaterial color="yellowgreen" />
+            </mesh>
+
+        </>
+  )
+}
+
+export default Experience
---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+// import { Leva } from 'leva'
+import { StrictMode } from 'react'
+import Experience from './Experience'
+import './style.css'
+
+
+
+const root = createRoot(document.querySelector('#root'))
+
+
+root.render(
+    <StrictMode>
+        {/* <Leva collapsed /> */}
+        <Canvas
+            // flat
+            camera={{
+            fov: 45,
+            position: [-4, 3, 6]
+            }}
+        >
+            <Experience />q
+        </Canvas>
+    </StrictMode>
+)
